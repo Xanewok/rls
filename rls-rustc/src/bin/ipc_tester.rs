66409d6529d5763eeed5bfbdde645a49a8d0e0a9
Igor Matuszewski <xanewok@gmail.com>
2019-07-16 22:08:44 +0200

    WIP: Try to use jsonrpc

--- rls-rustc/src/bin/ipc_tester.rs
+++ rls-rustc/src/bin/ipc_tester.rs
@@ -3,47 +3,67 @@
 
 use std::process::Command;
 
-use futures::{stream::Stream, Future, sync::oneshot};
+use futures::{stream::Stream, sync::oneshot, Future};
 use parity_tokio_ipc::{dummy_endpoint, Endpoint, SecurityAttributes};
 use tokio;
 use tokio::io::{self, AsyncRead};
+
+use jsonrpc_ipc_server::jsonrpc_core::*;
+use jsonrpc_ipc_server::ServerBuilder;
+use jsonrpc_derive::rpc;
+
+#[rpc]
+pub trait Rpc {
+    /// Returns a protocol version
+    #[rpc(name = "say_hello")]
+    fn say_hello(&self) -> Result<String>;
+}
+struct RpcImpl;
+impl Rpc for RpcImpl {
+    fn say_hello(&self) -> Result<String> { Ok(String::from("No siema")) }
+}
 
 fn main() {
     env_logger::init();
 
     let endpoint_path = dummy_endpoint();
-
-    let mut runtime = tokio::runtime::Runtime::new().expect("Error creating tokio runtime");
-    let executor = runtime.executor();
-
-    let (tx, rx) = oneshot::channel();
+    
     let thread = std::thread::spawn({
         let endpoint_path = endpoint_path.clone();
         move || {
+            let mut io = IoHandler::new();
+            io.add_method("say_hello", |_params| {
+                eprintln!("ipc_tester: At long fucking last");
+                Ok(serde_json::Value::String("No eloszka".into()))
+            });
+            // io.extend_with(RpcImpl.to_delegate());
 
-            let endpoint = Endpoint::new(endpoint_path.clone());
+            let builder = ServerBuilder::new(io);
+            let server = builder.start(&endpoint_path).expect("Couldn't open socket");
+            eprintln!("ipc_tester: About to wait on the IPC server");
+            server.wait();
 
-            let connections = endpoint
-                .incoming(&Default::default())
-                .expect("failed to open up a new pipe/socket");
-            let server = connections
-                .for_each(|(stream, _)| {
-                    eprintln!("ipc_tester: Client has connected!");
-                    let (reader, writer) = stream.split();
+            // let connections = endpoint
+            //     .incoming(&Default::default())
+            //     .expect("failed to open up a new pipe/socket");
+            // let server = connections
+            //     .for_each(|(stream, _)| {
+            //         eprintln!("ipc_tester: Client has connected!");
+            //         let (reader, writer) = stream.split();
 
-                        let buf = vec![0; 128];
-                        io::read(reader, buf)
-                            .map(|(_, buf, _)| eprintln!("ipc_tester: Server read {:?}", buf)).wait();
-                            Ok(())
-                    // io::write_all(writer, b"Server sending hello!").and_then(|_| {
-                    // })
-                })
-                .map_err(|_| ());
-            executor.spawn(server);
-            tx.send(()).expect("failed to send ok");
+            //             let buf = vec![0; 128];
+            //             io::read(reader, buf)
+            //                 .map(|(_, buf, _)| eprintln!("ipc_tester: Server read {:?}", buf)).wait();
+            //                 Ok(())
+            //         // io::write_all(writer, b"Server sending hello!").and_then(|_| {
+            //         // })
+            //     })
+            //     .map_err(|_| ());
+            // executor.spawn(server);
+            // tx.send(()).expect("failed to send ok");
         }
     });
-    rx.wait().expect("Failed to wait for the server");
+    // rx.wait().expect("Failed to wait for the server");
     eprintln!("ipc_tester: Server has been spawned");
 
     std::thread::sleep_ms(1000);
@@ -56,6 +76,6 @@ fn main() {
         .unwrap();
     dbg!(&output);
 
-    runtime.shutdown_on_idle().wait().unwrap();
+    // runtime.shutdown_on_idle().wait().unwrap();
     let _ = thread.join();
 }
--- rls-rustc/src/ipc.rs
+++ rls-rustc/src/ipc.rs
@@ -1,3 +1,8 @@
+use jsonrpc_core::types::params::Params;
+use jsonrpc_core_client::transports::duplex;
+use jsonrpc_core_client::RpcChannel;
+use jsonrpc_core_client::RpcError;
+use jsonrpc_core_client::TypedClient;
 use std::io;
 use std::path::{Path, PathBuf};
 
@@ -5,13 +10,81 @@ use crate::syntax::source_map::FileLoader;
 use futures::Future;
 use parity_tokio_ipc::IpcConnection;
 
+use tokio::io::AsyncRead;
+
+// use jsonrpc_client_core::jsonrpc_client;
+use jsonrpc_core::{Error, IoHandler, Result};
+use jsonrpc_core_client::transports::local;
+use jsonrpc_derive::rpc;
+
+#[rpc]
+pub trait Rpc {
+    /// Returns a protocol version
+    #[rpc(name = "say_hello")]
+    fn say_hello(&self) -> Result<String>;
+}
+
 pub struct IpcFileLoader;
 
 impl IpcFileLoader {
     pub fn new(path: String) -> io::Result<Self> {
+        let mut runtime = tokio::runtime::Runtime::new().unwrap();
+
         eprintln!("ipc: Attempting to connect to {}", path);
         let connection = IpcConnection::connect(path, &Default::default())?;
+        use futures::stream::Stream;
+        use futures::sink::Sink;
+        // let codec = jsonrpc_server_utilscodecs::StreamCodec::stream_incoming();
+        let codec = tokio::codec::LinesCodec::new();
+        let (sink, stream) = connection
+            .framed(codec)
+            .split();
+        let sink = sink.sink_map_err(|_| jsonrpc_core_client::RpcError::Timeout);
+        let stream = stream.map_err(|_| jsonrpc_core_client::RpcError::Timeout);
         eprintln!("ipc: Client connected");
+
+        eprintln!("ipc: Setting up duplex");
+        let (client, sender) = duplex(sink, stream);
+
+        let raw = jsonrpc_core_client::RawClient::from(sender);
+        eprintln!("ipc: Call say_hello");
+        let result = raw
+            .call_method("say_hello", Params::None)
+            .map(|val| {
+                eprintln!("ipc: Result of say_hello method: {:?}", val);
+            })
+            .map_err(|e| {
+                eprintln!("ipc: Called method say_hello failed with: {:?}", e)
+            });
+
+        // let core = tokio::reactor::Reactor::new().unwrap();
+        runtime.spawn(client.map_err(|_| ()));
+        let blocked_on = runtime.block_on(result);
+        dbg!(&blocked_on);
+
+        // let result = runtime.block_on(result).unwrap();
+        // dbg!(&result);
+
+        // struct TestClient(TypedClient);
+        // impl TestClient {
+        //     fn say_hello(&self) -> impl Future<Item = String, Error = RpcError> {
+        //         self.0.call_method("say_hello", "()", ())
+        //     }
+        //     // fn hello(&self, msg: &'static str) -> impl Future<Item = String, Error = RpcError> {
+        //     //     self.0.call_method("hello", "String", (msg,))
+        //     // }
+        //     // fn fail(&self) -> impl Future<Item = (), Error = RpcError> {
+        //     //     self.0.call_method("fail", "()", ())
+        //     // }
+        // }
+        // let test_client = TestClient(sender.into());
+        // let call_result = test_client.say_hello().wait().unwrap();
+        // dbg!(&call_result);
+        // let client = TClient::from(sender);
+        // (client, rpc_client)
+
+        // duplex::duplex(, stream: TStream)
+        // let (client, server) = duplex::connect::<gen_client::Client, _, _>(io);
 
         // let msg = "client here";
         // let rx_buf = vec![0u8; msg.len()];
@@ -24,8 +97,8 @@ impl IpcFileLoader {
         // eprintln!("ipc: Waiting on client_0");
         // client_0_fut.wait().unwrap();
 
-        let rx_buf2 = vec![0u8; 5];
-        tokio::io::write_all(connection, b"Dupaa").wait();
+        // let rx_buf2 = vec![0u8; 5];
+        // tokio::io::write_all(connection, b"Dupaa").wait();
         // tokio::io::read(connection, rx_buf2).map(|(_, buf, _)| dbg!(buf));
 
         // let fut = tokio::io::read(connection, rx_buf2)
