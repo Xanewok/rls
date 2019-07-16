aa7b7972f8c43ed9e4aea4d1458bed6bcd3a8884
Igor Matuszewski <xanewok@gmail.com>
2019-07-17 00:12:43 +0200

    WIP: Now actually server receives some data!

--- rls-rustc/src/bin/ipc_tester.rs
+++ rls-rustc/src/bin/ipc_tester.rs
@@ -17,51 +17,47 @@ fn main() {
 
     let endpoint_path = dummy_endpoint();
 
-    // let mut runtime = tokio::runtime::Runtime::new().unwrap();
+    let mut runtime = tokio::runtime::Runtime::new().unwrap();
+    let handle = runtime.reactor();
 
-    // let (tx, rx) = oneshot::channel::<()>();
-    // let thread = std::thread::spawn({
-    let endpoint_path = endpoint_path.clone();
-    // move || {
-    let mut io = IoHandler::new();
-    io.add_method("say_hello", |_params| {
-        eprintln!("ipc_tester: At long fucking last");
-        Ok(serde_json::Value::String("No eloszka".into()))
-    });
-
-    let builder = ServerBuilder::new(io);
-    let server = builder.start(&endpoint_path).expect("Couldn't open socket");
-    eprintln!("ipc_tester: Started an IPC server");
-
-    // let endpoint = Endpoint::new(endpoint_path);
-    // let connections = endpoint
-    //     .incoming(&Default::default())
-    //     .expect("failed to open up a new pipe/socket");
-    // let server = connections
-    //     .for_each(|(stream, _)| {
-    //         eprintln!("ipc_tester: Client has connected!");
-    //         let (reader, writer) = stream.split();
-
-    //             let buf = vec![0; 128];
-    //             io::read(reader, buf)
-    //                 .map(|(_, buf, _)| eprintln!("ipc_tester: Server read {:?}", buf)).wait();
-    //                 Ok(())
-    //         // io::write_all(writer, b"Server sending hello!").and_then(|_| {
-    //         // })
-    //     })
-    //     .map_err(|_| ());
-    // runtime.block_on(server);
-    // tx.send(()).expect("failed to send ok");
-    // }
+    // let mut io = IoHandler::new();
+    // io.add_method("say_hello", |_params| {
+    //     eprintln!("ipc_tester: At long fucking last");
+    //     Ok(serde_json::Value::String("No eloszka".into()))
     // });
-    // rx.wait().expect("Failed to wait for the server");
+
+    // let builder = ServerBuilder::new(io);
+    // let server = builder.start(&endpoint_path).expect("Couldn't open socket");
+    // eprintln!("ipc_tester: Started an IPC server");
+
+    let endpoint = Endpoint::new(endpoint_path.clone());
+    let server = endpoint
+        .incoming(handle)
+        .unwrap()
+        .for_each(|(connection, _)| {
+            eprintln!("ipc_tester: Client connected!");
+            let (reader, writer) = connection.split();
+            // TODO: Try reading using a StreamCodec instead of buffers
+            let buf = [0u8; 5];
+            io::read_exact(reader, buf)
+                .map(|(_, buf)| {
+                    let buf = String::from_utf8_lossy(&buf);
+                    eprintln!("Read some: `{:?}`", buf)
+                })
+                .map_err(|e| {
+                    eprintln!("io error: {:?}", e);
+                    e
+                })
+        })
+        .map_err(|_| ());
+    runtime.spawn(server);
     eprintln!("ipc_tester: Server has been spawned");
 
     std::thread::sleep_ms(1000);
 
     let mut child = Command::new("cargo")
         .args(&["run", "--bin", "rustc"])
-        // .env_remove("RUST_LOG")
+        .env_remove("RUST_LOG")
         .env("RLS_IPC_ENDPOINT", endpoint_path)
         .stderr(std::process::Stdio::inherit())
         .spawn()
