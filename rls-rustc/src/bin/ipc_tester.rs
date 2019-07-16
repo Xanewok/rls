44d1f650e160b0cff640b30d6923018500323abd
Igor Matuszewski <Xanewok@gmail.com>
2019-07-16 13:28:36 +0200

    WIP

--- rls-rustc/src/bin/ipc_tester.rs
+++ rls-rustc/src/bin/ipc_tester.rs
@@ -3,38 +3,59 @@
 
 use std::process::Command;
 
-use futures::{stream::Stream, Future};
-use parity_tokio_ipc::{dummy_endpoint, Endpoint};
+use futures::{stream::Stream, Future, sync::oneshot};
+use parity_tokio_ipc::{dummy_endpoint, Endpoint, SecurityAttributes};
 use tokio;
 use tokio::io::{self, AsyncRead};
 
 fn main() {
-    let endpoint = dummy_endpoint();
+    env_logger::init();
 
-    std::thread::spawn({
-        let endpoint = endpoint.clone();
+    let endpoint_path = dummy_endpoint();
+
+    let mut runtime = tokio::runtime::Runtime::new().expect("Error creating tokio runtime");
+    let executor = runtime.executor();
+
+    let (tx, rx) = oneshot::channel();
+    let thread = std::thread::spawn({
+        let endpoint_path = endpoint_path.clone();
         move || {
-            let mut runtime = tokio::runtime::Runtime::new().expect("Can't create Runtime");
 
-            let endpoint = Endpoint::new(endpoint);
+            let endpoint = Endpoint::new(endpoint_path.clone());
+
             let connections = endpoint
                 .incoming(&Default::default())
                 .expect("failed to open up a new pipe/socket");
             let server = connections
                 .for_each(|(stream, _)| {
-                    eprintln!("Connected!");
-                    let (_, writer) = stream.split();
-                    io::write_all(writer, b"Hello!").map(|_| ())
+                    eprintln!("ipc_tester: Client has connected!");
+                    let (reader, writer) = stream.split();
+
+                        let buf = vec![0; 128];
+                        io::read(reader, buf)
+                            .map(|(_, buf, _)| eprintln!("ipc_tester: Server read {:?}", buf)).wait();
+                            Ok(())
+                    // io::write_all(writer, b"Server sending hello!").and_then(|_| {
+                    // })
                 })
                 .map_err(|_| ());
-            runtime.block_on(server).unwrap();
+            executor.spawn(server);
+            tx.send(()).expect("failed to send ok");
         }
     });
+    rx.wait().expect("Failed to wait for the server");
+    eprintln!("ipc_tester: Server has been spawned");
+
+    std::thread::sleep_ms(1000);
 
     let output = Command::new("cargo")
         .args(&["run", "--bin", "rustc"])
-        .env("RLS_IPC_ENDPOINT", endpoint)
+        .env("RLS_IPC_ENDPOINT", endpoint_path)
+        .stderr(std::process::Stdio::inherit())
         .output()
         .unwrap();
     dbg!(&output);
+
+    runtime.shutdown_on_idle().wait().unwrap();
+    let _ = thread.join();
 }
--- rls-rustc/src/ipc.rs
+++ rls-rustc/src/ipc.rs
@@ -9,17 +9,32 @@ pub struct IpcFileLoader;
 
 impl IpcFileLoader {
     pub fn new(path: String) -> io::Result<Self> {
-        let mut runtime = tokio::runtime::Runtime::new().expect("Error creating tokio runtime");
-
+        eprintln!("ipc: Attempting to connect to {}", path);
         let connection = IpcConnection::connect(path, &Default::default())?;
+        eprintln!("ipc: Client connected");
+
+        // let msg = "client here";
+        // let rx_buf = vec![0u8; msg.len()];
+        // let client_0_fut = tokio::io::write_all(connection, msg)
+        //     .map_err(|err| panic!("Client 0 write error: {:?}", err))
+        //     .and_then(move |(client, _)| {
+        //         tokio::io::read_exact(client, rx_buf).map(|(_, buf)| buf)
+        //             .map_err(|err| panic!("Client 0 read error: {:?}", err))
+        //     });
+        // eprintln!("ipc: Waiting on client_0");
+        // client_0_fut.wait().unwrap();
 
         let rx_buf2 = vec![0u8; 5];
-        let fut = tokio::io::read_exact(connection, rx_buf2)
-            .map(|(_, buf)| buf)
-            .map_err(|err| panic!("Client 1 read error: {:?}", err));
+        tokio::io::write_all(connection, b"Dupaa").wait();
+        // tokio::io::read(connection, rx_buf2).map(|(_, buf, _)| dbg!(buf));
 
-        let test_buf = runtime.block_on(fut).unwrap();
-        eprintln!("Read from IPC: `{}`", String::from_utf8(test_buf).unwrap());
+        // let fut = tokio::io::read(connection, rx_buf2)
+        //     .map(|(_conn, buf, _bytes)| buf)
+        //     .map_err(|err| panic!("Client 1 read error: {:?}", err));
+        // eprintln!("Waiting on test_buf");
+        // let test_buf = fut.wait().unwrap();
+
+        // eprintln!("Read from IPC: `{}`", String::from_utf8(test_buf).unwrap());
 
         Ok(IpcFileLoader)
     }
